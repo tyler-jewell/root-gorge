632ef6723159b5ceb01e4e9ce3fde6f6c2ed67e6
fogoops <fogoops@gmail.com>
2022-02-26 01:07:01 -0700

    chore: changing to mez ops

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,5 +1,5 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
+// Copyright (c) 2022, Mezops
+// https://mezops.com
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
